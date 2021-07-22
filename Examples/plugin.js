f8d6535185162e9233244794db0bf1e166514c09
Deiv <77246331+dubisdev@users.noreply.github.com>
2021-07-22 19:28:24 +0200

    Create plugin.js

---
+++ Examples/plugin.js
@@ -0,0 +1,33 @@
+import icon from "./icons";
+import CerebroRouter from "./cerebro-command-routing/index.js";
+
+if (!Notification.permission) Notification.requestPermission();
+
+function fn({ term, display, actions }) {
+		const myRouter = new CerebroRouter({ command: "example", term, display });
+
+		myRouter.route("com1", {
+			icon: icon,
+			title: `Example Plugin command 1`,
+			getPreview: () => <h2>This is command 1 :)</h2>,
+			onSelect: () => new Notification("You selected subcommand 1"),
+		});
+
+		myRouter.route("com2", {
+			icon: icon,
+			title: `Example Plugin command 2`,
+			getPreview: () => <h2>This is command 2 :)</h2>,
+		});
+
+		myRouter.invalidRoute({
+			icon: icon,
+			title: `Invalid Example Command`,
+		});
+}
+
+// ----------------- Plugin settings --------------------- //
+const name = "Example Plugin";
+const keyword = "example";
+// ----------------- END Plugin settings --------------------- //
+
+export { icon, name, keyword, fn,  };
